29894248102d855f386d76d047fd5d95c2c60d0f
mt <martinli.work@gmail.com>
2022-01-17 21:40:44 -0500

    Bump ver to 0.1.0

--- nntime/__init__.py
+++ nntime/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.0.2'
+__version__ = '0.1.0'
 
 from .timing import set_global_depth, get_global_depth, \
     set_global_sync, get_global_sync, \
@@ -10,4 +10,3 @@ __all__ = [
     'time_this', 'timer_start', 'timer_end',
     'export_timings',
 ]
-
